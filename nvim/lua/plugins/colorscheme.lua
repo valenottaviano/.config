966f373f09a0831a4629699ac1b5798481bd5e90
Valentín Ottaviano <valentinottaviano@gmail.com>
2024-05-12 20:34:33 -0300

    initial commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { 'tjdevries/colorbuddy.nvim' },
+  {
+    "jesseleite/nvim-noirbuddy",
+    requires = { "tjdevries/colorbuddy.nvim" }
+  },
+  { 'kdheepak/monochrome.nvim' },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "noirbuddy",
+    },
+  }
+}
